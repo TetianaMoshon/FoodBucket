87b83869c9f8a5f49329c75604cb00c0e6ee8736
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-07-18 17:29:42 +0300

    Finished feature routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { Routes, RouterModule} from '@angular/router';
 import { ModuleWithProviders} from '@angular/core';
 import {HomeComponent} from './components/home/home.page.component';
 import {IngredientsComponent} from './components/ingredients/ingredients.component';
+import {CategoryComponent} from './components/category/category.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'ingredients', component: IngredientsComponent}
+  { path: 'ingredients', component: IngredientsComponent},
+  { path: 'category', component: CategoryComponent}
 ];
 
 export  const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
