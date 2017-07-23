396e4bfe2d0649361437cb5f299898aa6b06b8a6
Ivan Liutko <ivanliutko@gmail.com>
2017-07-23 17:16:23 +0300

    Installed as dependencies bootstrap and ngx-bootstrap, added slider component and photos into assets/images/nature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ngx-bootstrap
+import { CarouselModule } from 'ngx-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.page.component';
 import { IngredientsComponent } from './components/ingredients/ingredients.component';
 import { CategoryComponent } from './components/category/category.component';
 import {appRouting} from './app.routing';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     IngredientsComponent,
-    CategoryComponent
+    CategoryComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    appRouting
+    appRouting,
+      // ngx-bootstrap
+      CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
