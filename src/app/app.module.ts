b1cff8b24e3f8cd3aaa12f0dcf92e970c8816a50
oleksii.pimenov <a.pimenov@yahoo.com>
2017-07-25 18:07:07 +0300

    Added nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { ModalModule } from 'ngx-bootstrap/modal';
-
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.page.component';
 import { IngredientsComponent } from './components/ingredients/ingredients.component';
 import { CategoryComponent } from './components/category/category.component';
 import {appRouting} from './app.routing';
 import { ModalCongratulationComponent } from './components/modals/modal-congratulation/modal-congratulation.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +16,17 @@ import { ModalCongratulationComponent } from './components/modals/modal-congratu
     HomeComponent,
     IngredientsComponent,
     CategoryComponent,
-    ModalCongratulationComponent
+    ModalCongratulationComponent,
+    NavbarComponent
   ],
   entryComponents: [
     ModalCongratulationComponent
   ],
   imports: [
-    ModalModule.forRoot(),
-    BrowserModule,
-    appRouting
+      ModalModule.forRoot(),
+      BrowserModule,
+      CollapseModule,
+      appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
