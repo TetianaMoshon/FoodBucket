a2af7638b1e5212013698845f3e846558c1a603e
Katya <kvto4ka1@gmail.com>
2017-07-31 16:43:10 +0300

    add style

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { ModuleWithProviders} from '@angular/core';
 import {HomeComponent} from './components/home/home.page.component';
 import {IngredientsComponent} from './components/ingredients/ingredients.component';
 import {CategoryComponent} from './components/category/category.component';
+import {AboutusComponent} from './components/aboutus/aboutus.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'ingredients', component: IngredientsComponent},
-  { path: 'category', component: CategoryComponent}
+  { path: 'category', component: CategoryComponent},
+  { path: 'aboutus', component: AboutusComponent}
 ];
 
 export  const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
