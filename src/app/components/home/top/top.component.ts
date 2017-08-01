8e49a388def989d5f9c44f42d49dbb1d2cad4a04
tanya.moshon <t.moshon@gmail.com>
2017-08-01 10:04:13 +0300

    Created top rated component

--- src/app/components/home/top/top.component.ts
+++ src/app/components/home/top/top.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-top',
-  templateUrl: './top.component.html',
-  styleUrls: ['./top.component.css']
+    selector: 'app-top',
+    templateUrl: './top.component.html',
+    styleUrls: ['./top.component.css']
 })
 export class TopComponent implements OnInit {
 
-  constructor() { }
+    quantityOfPhotos: number;
 
-  ngOnInit() {
-  }
+    sourceForPreviousImage: any;
+    sourceForNextImage: any;
+
+    counter = 1;
+
+    initialSourceForPreviousImage = 0;
+    initialSourceForNextImage = 1;
+
+    ListOfImageLinks: string [] = [
+        './assets/images/strawberry-pancakes.jpg',
+        './assets/images/cake-pops.jpg',
+        './assets/images/cherry-streusel-cheesecake.jpg'
+    ];
+
+    constructor() {}
+
+    ngOnInit() {
+        this.InitImageSource();
+        this.quantityOfPhotos = this.ListOfImageLinks.length;
+    }
+
+    InitImageSource() {
+        this.sourceForPreviousImage = this.ListOfImageLinks[this.initialSourceForPreviousImage];
+        this.sourceForNextImage = this.ListOfImageLinks[this.initialSourceForNextImage];
+    }
+
+    changeImageSourceByPressingRightArrow() {
+        this.slideToTheRight();
+    }
+
+    changeImageSourceByPressingLeftArrow() {
+        this.slideToTheLeft();
+    }
+
+    slideToTheRight() {
+        this.sourceForNextImage = this.ListOfImageLinks[this.initialSourceForPreviousImage];
+        this.sourceForPreviousImage = this.ListOfImageLinks[this.initialSourceForNextImage];
+
+        const temp3 = this.initialSourceForNextImage;
+        this.initialSourceForNextImage = (this.initialSourceForNextImage + this.counter) % this.quantityOfPhotos;
+        const temp2 = this.initialSourceForPreviousImage;
+        this.initialSourceForPreviousImage = temp2;
+
+    }
+
+    slideToTheLeft() {
+        this.sourceForPreviousImage = this.ListOfImageLinks[this.initialSourceForNextImage];
+        this.sourceForNextImage = this.ListOfImageLinks[this.initialSourceForPreviousImage];
+        this.sourceForPreviousImage = this.ListOfImageLinks[ this.initialSourceForPreviousImage === 0  ? this.quantityOfPhotos - this.counter :
+            this.initialSourceForPreviousImage - this.counter
+            ];
+
+        this.initialSourceForNextImage = this.initialSourceForPreviousImage;
+        this.initialSourceForPreviousImage = this.initialSourceForPreviousImage === 0  ? this.quantityOfPhotos - this.counter :
+            this.initialSourceForPreviousImage - this.counter;
+    }
 
 }
