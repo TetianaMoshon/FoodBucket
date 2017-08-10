75b6775053150da6b85fb0ae800dd3cde692bb2c
Nikita Korba <no.korba@gmail.com>
2017-08-10 17:56:40 +0300

    Created users table

--- src/app/components/admin-orders/admin-orders.component.ts
+++ src/app/components/admin-orders/admin-orders.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-orders.component.css']
 })
 export class AdminOrdersComponent implements OnInit {
-
+array = [{
+    "date": "27/05/2015",
+    "id": "1",
+    "name": "John",
+    "food": "pizza,pasta,salad",
+    "tel": "0682839534",
+    "price": "13,37$",
+    "status": "delivered",
+},
+    {
+        "date": "27/05/2015",
+        "id": "2",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "3",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "4",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "5",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    }];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
