4c1d449ade8bddffa0a1d25dee3b2fd41ce49357
Nikita Korba <no.korba@gmail.com>
2017-08-11 17:48:40 +0300

    Added pagination to the component

--- src/app/components/admin-orders/admin-orders.component.ts
+++ src/app/components/admin-orders/admin-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {PagerService} from "../../services/pagination.service";
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
@@ -50,11 +50,156 @@ array = [{
         "tel": "0682839534",
         "price": "13,37$",
         "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "6",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "7",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "8",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "9",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "10",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "11",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "12",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "13",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "14",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "15",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "16",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "17",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "18",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "19",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
+    },
+    {
+        "date": "27/05/2015",
+        "id": "20",
+        "name": "John",
+        "food": "pizza,pasta,salad",
+        "tel": "0682839534",
+        "price": "13,37$",
+        "status": "delivered",
     }];
-  constructor() { }
+pager: any = {};
+pagedItems: any[];
+
+  constructor(private pagerService:PagerService) { }
 
   ngOnInit() {
-
+    this.setPage(1);
   }
+setPage(page:number) {
+      if (page<1 || page> this.pager.totalPages) {
+          return;
+      }
 
+      this.pager = this.pagerService.getPager(this.array.length,page);
+      this.pagedItems = this.array.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
 }
