b1589d8fc512d4d4b4fc7093d14554ada1120fce
Anastasiia Bilashevs'ka <abilastc@training.local>
2017-08-18 17:48:58 +0300

    add charts and basic styles

--- src/app/components/admin-analytics/admin-analytics.component.ts
+++ src/app/components/admin-analytics/admin-analytics.component.ts
@@ -14,7 +14,7 @@ export class AdminAnalyticsComponent implements OnInit {
     ngOnInit() {
     }
 
-    // lineChart
+    //Charts
     public lineChartData:Array<any> = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
@@ -22,7 +22,8 @@ export class AdminAnalyticsComponent implements OnInit {
     ];
     public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     public lineChartOptions:any = {
-        responsive: true
+        responsive: true,
+        width: '250px'
     };
     public lineChartColors:Array<any> = [
         { // grey
@@ -52,18 +53,25 @@ export class AdminAnalyticsComponent implements OnInit {
     ];
     public lineChartLegend:boolean = true;
     public lineChartType:string = 'line';
+    // Doughnut
+    // Doughnut
+    public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    public doughnutChartData:number[] = [350, 450, 100];
+    public doughnutChartType:string = 'doughnut';
 
-    public randomize():void {
-        let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-        for (let i = 0; i < this.lineChartData.length; i++) {
-            _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-            }
-        }
-        this.lineChartData = _lineChartData;
-    }
+    //bar
+    public barChartOptions:any = {
+        scaleShowVerticalLines: false,
+        responsive: true
+    };
+    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartType:string = 'bar';
+    public barChartLegend:boolean = true;
 
+    public barChartData:any[] = [
+        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    ];
     // events
     public chartClicked(e:any):void {
         console.log(e);
@@ -72,6 +80,5 @@ export class AdminAnalyticsComponent implements OnInit {
     public chartHovered(e:any):void {
         console.log(e);
     }
-
 }
 
