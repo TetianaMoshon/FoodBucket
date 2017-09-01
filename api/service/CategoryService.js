37597edf52f49389b238f9f9c5618d55573a59fe
Vitaly Bessarab <vitaly.bessarab@gmail.com>
2017-09-01 19:48:06 +0300

    CategoryService.js refactoring

--- api/service/CategoryService.js
+++ api/service/CategoryService.js
@@ -1,4 +1,5 @@
 'use strict';
+const utils = require('../utils/writer.js');
 const Category = require('../model/category');
 
 /**
@@ -8,9 +9,8 @@ const Category = require('../model/category');
  * body Category Category object
  * returns Category
  **/
-exports.createCategory = function (body) {
+exports.createCategory = function ({ category_id, title, image, description }) {
     return new Promise((resolve, reject) => {
-        let { category_id, title, image, description } = body;
         let newCategory = new Category({
             "category_id": category_id,
             "title": title,
@@ -21,16 +21,17 @@ exports.createCategory = function (body) {
         newCategory.save().then(
             categoryDoc => {
                 if (Object.keys(categoryDoc).length > 0) {
-                    resolve(categoryDoc);
+                    let {category_id, title, image, description} = categoryDoc;
+                    resolve(utils.respondWithCode(201, {category_id, title, image, description}));
                 } else {
-                    reject();
+                    reject(utils.respondWithCode(404, {"code": 404, "message": "Category is not created, please try again."}));
                 }
                 console.log('Saved category', categoryDoc);
             },
             error => { console.log('Unable to save category: ', error); }
         );
     });
-}
+};
 
 
 /**
@@ -44,15 +45,17 @@ exports.deleteCategoryById = function(id) {
             oneCategoryDoc => {
                 oneCategoryDoc = oneCategoryDoc || {};
                 if (Object.keys(oneCategoryDoc).length > 0) {
-                    resolve(oneCategoryDoc);
+                    let {category_id, title, image, description} = oneCategoryDoc;
+                    // if we need to return content we should use code 200
+                    resolve(utils.respondWithCode(204, {category_id, title, image, description}));
                 } else {
-                    reject();
+                    reject(utils.respondWithCode(404, {"code": 404, "message": "Category is not deleted, please try again."}));
                 }
             },
             error => { console.log('Unable to remove category: ', error); }
         );
     });
-}
+};
 
 
 /**
@@ -66,15 +69,16 @@ exports.findCategoryById = function (id) {
             oneCategoryDoc => {
                 oneCategoryDoc = oneCategoryDoc || {};
                 if (Object.keys(oneCategoryDoc).length > 0) {
-                    resolve(oneCategoryDoc);
+                    let {category_id, title, image, description} = oneCategoryDoc;
+                    resolve(utils.respondWithCode(200, {category_id, title, image, description}));
                 } else {
-                    reject();
+                    reject(utils.respondWithCode(404, {"code": 404, "message": "Category is not found, please try again."}));
                 }
             },
             error => { console.log('Unable to get category: ', error); }
         );
     });
-}
+};
 
 
 /**
@@ -90,15 +94,18 @@ exports.getAllCategories = function (offset, limit, isActive) {
             categoriesDoc => {
                 categoriesDoc = categoriesDoc || [];
                 if (Object.keys(categoriesDoc).length > 0) {
-                    resolve(categoriesDoc);
+                    categoriesDoc = categoriesDoc.map( ({ category_id, title, image, description }) => {
+                        return { category_id, title, image, description };
+                    });
+                    resolve(utils.respondWithCode(200, categoriesDoc));
                 } else {
-                    reject();
+                    reject(utils.respondWithCode(404, {"code": 404, "message": "Categories are not found, please try again."}));
                 }
             },
             error => { console.log('Unable to get categories: ', error); }
         );
     });
-}
+};
 
 
 /**
@@ -112,14 +119,15 @@ exports.updateCategoryById = function(id, updatedCategory) {
         let { title, image, description } = updatedCategory;
 
         Category.findOneAndUpdate({ category_id: id }, { title, image, description }).then(
-            () => {
-                if (Object.keys(updatedCategory).length > 0) {
-                    resolve(updatedCategory);
+            oneCategory => {
+                if (Object.keys(updatedCategory).length > 0 && oneCategory !== null) {
+                    let categoryId = oneCategory.category_id;
+                    resolve(utils.respondWithCode(200, {categoryId, title, image, description}));
                 } else {
-                    reject();
+                    reject(utils.respondWithCode(400, {"code": 404, "message": "Category is not updated, please try again."}));
                 }
             },
             error => { console.log('Unable to get category: ', error); }
         );
     });
-}
+};
