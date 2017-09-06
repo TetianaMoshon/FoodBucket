f653de05e917c09ab4802dcb02e2430a1876ddd0
bilashevska <bilashevska@gmail.com>
2017-09-06 12:58:39 +0300

    add some versions fo connect

--- api/service/ProductService.js
+++ api/service/ProductService.js
@@ -1,18 +1,40 @@
 'use strict';
 const utils = require('../utils/writer.js');
 const Product = require('../model/product');
-const Ingredient = require('../model/ingredients');
+const Ingredient = require('../service/IngredientService');
 
 exports.createProduct = function (body) {
     return new Promise((resolve, reject) => {
         let { productId, title, description, image, price, category, caloricity, servingSize, difficulty, spiceLevel, recommended, discount, promotions, status, ingredients} = body;
 
-        for(let i = 0; i < ingredients.length;i++){
-            Ingredient.findOne({'ingredient_id':ingredients[i].ingredientId}, function (err,obj) {
-                doc.ingredients[i].ingredientName = obj.title;
-            })};
+        //with promises
+        // let temp = Ingredient.findIngredientById(32)
+        //     .then(
+        //         oneIngredientDoc =>{
+        //             let doc = oneIngredientDoc;
+        //             console.log(doc);
+        //             return doc;
+        //         },
+        //         error => {return error}
+        //         );
+        // console.log(temp);
 
-        var temporary = ingredients;
+        //with mongoose
+        // function findIngredient(id) {
+        //     let query = Ingredient.findOne({ingredient_id:id});
+        //     return query;
+        // }
+        // let query = findIngredient(ingredients[0].ingredientId);
+        // query.exec(function (err, obj) {
+        //     if(err)
+        //         return console.log(err);
+        //     jedis.forEach(function(jedi){
+        //         console.log(jedi.name);
+        //     });
+        // })
+
+
+
         let newProduct = new Product({
             "productId": productId,
             "title": title,
@@ -31,10 +53,9 @@ exports.createProduct = function (body) {
             "ingredients": ingredients
         });
 
-        Product.update(all, {'difficulty': 'WORK'});
 
         newProduct.save().then(
-            productDoc => { console.log('Saved product', temporary); },
+            productDoc => { console.log('Saved product', productDoc); },
             error => { console.log('Unable to save product', error); }
         );
 
