baf988edcd66eea67127beead3101fbcb7097aee
Nikita Korba <no.korba@gmail.com>
2017-09-09 18:05:50 +0300

    Adding validation to checkout card number

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+      function validateCardNumber(value) {
+          const v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+          const matches = v.match(/\d{4,16}/g);
+          const match = matches && matches[0] || '';
+          const parts = [];
+          for (let i = 0, len = match.length; i < len; i += 4) {
+              parts.push(match.substring(i, i + 4));
+          }
+          if (parts.length) {
+              return parts.join(' ');
+          } else {
+              return value;
+          }
+      }
+          document.getElementById('card').addEventListener('input', function () {
+              this.value = validateCardNumber(this.value);
+          });
   }
 
 }
