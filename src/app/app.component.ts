15bf1a37c0f2e327462b279edd2b3ceaa36e5707
Nikita Korba <no.korba@gmail.com>
2017-09-15 17:54:20 +0300

    Hide Slider and Footer on admin part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    constructor(public  _router: Router) {
+    }
     ngOnInit() {
         localStorage.setItem('showAPhrase', JSON.stringify(true));
         localStorage.setItem('cartContentObjCreated', JSON.stringify(false));
     }
-
-    title = 'app';
-
 }
