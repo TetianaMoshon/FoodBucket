d7647752f5b91f380b9025f34eff3d13f7a6b3b0
oleksii.pimenov <a.pimenov@yahoo.com>
2017-09-25 19:12:59 +0300

    version for stash

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -38,4 +38,18 @@ export class NavbarComponent implements OnInit {
     public openCartModal() {
         this.modalService.show(CartBoxComponent);
     }
+
+
+    public items: string[] = ['The first choice!',
+        'And another choice for you.', 'but wait! A third!'];
+
+    public onHidden(): void {
+        console.log('Dropdown is hidden');
+    }
+    public onShown(): void {
+        console.log('Dropdown is shown');
+    }
+    public isOpenChange(): void {
+        console.log('Dropdown state is changed');
+    }
 }
