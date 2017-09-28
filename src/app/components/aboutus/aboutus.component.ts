61792b9395ff804143d9f5a4e7b3aefac10251f7
Kvitka Katya <kvito4ka1@gmail.com>
2017-09-28 16:40:16 +0300

    fix about us page

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -10,13 +10,13 @@ import { Massage } from './massage';
 })
 export class AboutusComponent implements OnInit {
 
-  model = new Massage('', '', '', '');
-
   title = 'Our office';
   our_email = 'foodbucket@gmail.com';
   phone = '8-888-888-888';
   time = '8.00-22.00';
-
+  address = 'Degtyarivska St., 33B';
+  city = 'Kiev';
+  index = '02000';
   lat = 50.454660;
   lng = 30.52380;
 
