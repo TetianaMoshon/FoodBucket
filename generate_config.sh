8e231a47d64a6f0a03a74e2e405f13d85ca403f1
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2017-10-05 19:46:56 +0300

    fixed persist path

--- generate_config.sh
+++ generate_config.sh
@@ -2,7 +2,7 @@
 
 echo "Generating Docker Compose Env file"
 sed -e "s;%USERNAME%;$1;g" -e "s;%PASSWORD%;$2;g" \
-    -e "s;%DATABASE%;$3;g" \
+    -e "s;%DATABASE%;$3;g" -e "s;%PATH%;$5;g" \
     env.template > .env
 
 echo "Generating API Config"
